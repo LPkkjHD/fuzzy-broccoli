e52fc1b21fdff8faa8ffcff0839b54fb650cbcd1
Jakob Fassunge <87950162+dolerich-hirnfiedler@users.noreply.github.com>
2025-04-29 19:52:03 +0200

    added PlayerChunkUpdateEvent to app

--- src/map_genreation/mod.rs
+++ src/map_genreation/mod.rs
@@ -1,24 +1,29 @@
-use std::time::Duration;
+use crate::map_genreation::components::ResetTerrainEvent;
+use crate::map_genreation::resources::{CurrentChunks, GenerationSeed, GroundTiles};
+use crate::map_genreation::systems::{
+    clean_ground_tiles, despawn_chunks, handle_player_chunk_update_event,
+    handle_terrain_reset_event,
+};
+use crate::player::components::PlayerChunkUpdateEvent;
 use bevy::app::{App, Plugin, Update};
 use bevy::prelude::IntoSystemConfigs;
 use bevy::time::common_conditions::on_timer;
 use bevy::utils::{HashMap, HashSet};
 use rand::Rng;
-use crate::map_genreation::components::ResetTerrainEvent;
-use crate::map_genreation::resources::{CurrentChunks, GenerationSeed, GroundTiles};
-use crate::map_genreation::systems::{clean_ground_tiles, despawn_chunks, handle_player_chunk_update_event, handle_terrain_reset_event};
+use std::time::Duration;
 
+mod components;
 mod config;
 mod resources;
 mod systems;
 pub mod util;
-mod components;
 
 pub struct TerrainPlugin;
 
 impl Plugin for TerrainPlugin {
     fn build(&self, app: &mut App) {
         let mut rng = rand::thread_rng();
+        app.add_event::<PlayerChunkUpdateEvent>();
         app.insert_resource(GroundTiles(HashSet::new()))
             .insert_resource(CurrentChunks(HashMap::new()))
             .insert_resource(GenerationSeed(rng.gen()))
@@ -32,3 +37,4 @@ impl Plugin for TerrainPlugin {
             .add_event::<ResetTerrainEvent>();
     }
 }
+
